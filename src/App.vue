cff2124e90cce19ff6b739c61a4f368ff760802e
zhurunov.rodion <zhurunov.rodion@yandex.ru>
2018-05-09 23:57:10 +0500

    do result screen

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,11 @@
   <div class="training">
     <h1>Math training</h1>
     <hr>
+    <div class="progress">
+      <div class="progress-bar" :style="progressStyles"></div>
+    </div>
     <div class="box">
-      <transition name="flip">
+      <transition name="flip" mode="out-in">
         <app-start-screen
           v-if="state == 'start'"
           @onStart="onStart"
@@ -25,6 +28,8 @@
 
         <app-result-screen
           v-else-if="state == 'result'"
+          :stats="stats"
+          @repeat="onStart"
         ></app-result-screen>
 
         <div v-else>Unknown state</div>
@@ -47,37 +52,57 @@
         message:{
           type: '',
           text: ''
-        }
+        },
+        questMax: 3
       }
     },
     computed:{
         questDone(){
           return this.stats.success + this.stats.error;
-        }
+        },
+      progressStyles(){
+          return{
+            width: (this.questDone / this.questMax * 100) + '%'
+          }
+      }
 
     },
     methods:{
       onStart(){
         this.state = 'question';
+        this.stats.success = 0;
+        this.stats.error =0;
       },
       onQuestionSuccess(){
         this.state = 'message';
-        this.message.text='Good Job';
+        this.message.text='Good Job, but you are vse ravno pidor';
         this.message.type='success';
+        this.stats.success++;
       },
       onQuestionError(msg){
         this.state = 'message';
         this.message.text = msg;
         this.message.type='warning';
+        this.stats.error++;
       },
       onNext(){
-        this.state = 'question';
+        if(this.questDone < this.questMax){
+          this.state = 'question';
+        }else{
+          this.state = 'result';
+        }
+
       }
     }
   }
 </script>
 
 <style scoped>
+
+  .box{
+    margin-top: 15px;
+
+  }
  .training{
    max-width: 700px;
    margin: 20px auto;
--- src/components/ResultScreen.vue
+++ src/components/ResultScreen.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>4</div>
+  <div class="alert alert-primary">
+    <h3>Success: {{stats.success}}</h3>
+    <h3>Errors: {{stats.error}}</h3>
+    <h2>Don't delude yourself. Your mamka is fat!</h2>
+    <hr>
+    <button class="btn btn-primary" @click="$emit('repeat')">Repeat</button>
+    <button class="btn btn-primary" @click="$emit('nextLevel')">Next Level</button>
+  </div>
 </template>
 
 <script>
   export default {
-
+      props:['stats']
   }
 </script>
 
